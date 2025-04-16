42fe60e89bfb2e3bb26f69b7df5d49f04b3e1935
RickY <631797108@163.com>
2025-04-16 14:05:35 +0800

    Add postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-pxtorem': {
+      rootValue: 16,
+      propList: ['*'],
+      selectorBlackList: [],
+    },
+  },
+} 
